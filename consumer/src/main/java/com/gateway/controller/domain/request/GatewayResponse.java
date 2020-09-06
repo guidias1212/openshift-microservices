49f52d4a56781b17564a4c0022f69f638950ce74
Guilherme <gemdias@hotmail.com>
2020-09-06 12:46:52 -0300

    Added frontend code

---
+++ consumer/src/main/java/com/gateway/controller/domain/request/GatewayResponse.java
@@ -0,0 +1,15 @@
+package com.gateway.controller.domain.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GatewayResponse {
+
+    @JsonProperty("notes")
+    private List<String> notes;
+}
