eedbfbf4077cf5440011f4961d97fc10d3596162
Guilherme <gemdias@hotmail.com>
2020-11-02 13:31:23 -0300

    Adding builder annotation

--- authentication/src/main/java/com/auth/authentication/domain/DbEntity.java
+++ authentication/src/main/java/com/auth/authentication/domain/DbEntity.java
@@ -1,5 +1,6 @@
 package com.auth.authentication.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import javax.persistence.Entity;
 
 @Getter
 @Setter
+@Builder
 @Entity
 public class DbEntity {
 
