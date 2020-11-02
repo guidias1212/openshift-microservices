12c0802feeab3957019d7141ca1856a24d0f3ab1
Guilherme <gemdias@hotmail.com>
2020-11-02 15:51:34 -0300

    Adding TODO for next steps

--- authentication/src/main/java/com/auth/authentication/facade/TokenFacade.java
+++ authentication/src/main/java/com/auth/authentication/facade/TokenFacade.java
@@ -4,9 +4,15 @@ import com.auth.authentication.Repository.DbEntityRepository;
 import com.auth.authentication.domain.DbEntity;
 import com.auth.authentication.domain.Request;
 import com.auth.authentication.domain.Response;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.interfaces.RSAKeyProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.List;
 
 @Component
@@ -17,14 +23,21 @@ public class TokenFacade {
 
     public Response generateToken (Request request) {
 
-        List<DbEntity> dbEntityList = dbEntityRepository.findByUsernameAndPrivateKey(request.getUser(), request.getPublicKey());
+        //TODO: Create methods for hashing and de-hashing the password on Database
+        // https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/crypto/bcrypt/BCrypt.html
+        final String user = request.getUser();
+        final String publicKey = request.getPublicKey();
+
+        List<DbEntity> dbEntityList = dbEntityRepository.findByUsernameAndPrivateKey(user, publicKey);
 
         if (dbEntityList.size() > 0) {
-            String token = "12341234";
+            Algorithm algorithm = Algorithm.HMAC256(publicKey);
+            String token = JWT.create()
+                    .withIssuer(user)
+                    .sign(algorithm);
             String message = "Token successfully created";
             Response response = Response.builder().token(token).message(message).build();
             return response;
-
         } else {
             Response response = Response.builder().token("").message("Invalid Username or Password").build();
             return response;
